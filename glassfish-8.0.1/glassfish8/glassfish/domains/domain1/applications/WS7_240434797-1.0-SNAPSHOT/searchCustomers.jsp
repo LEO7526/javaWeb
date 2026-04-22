c0cb670fbe64c543edc3ff13ebfb4b6a191afc4b
unknown <a1@A306-004.vtc.hk>
2026-04-22 10:39:26 +0800

    .

--- glassfish-8.0.1/glassfish8/glassfish/domains/domain1/applications/WS7_240434797-1.0-SNAPSHOT/searchCustomers.jsp
+++
@@ -1,6 +0,0 @@
-<h1>Search Customer</h1>
-<form method="get" action="handleCustomer">
-    <input type="hidden" name="action" value="search" />
-    Name: <input type="text" name="name" />
-    <input type="submit" value="Search" />
-</form>
--- glassfish-8.0.1/glassfish8/mq/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.sun.messaging"},{"l":"com.sun.messaging.jms"},{"l":"com.sun.messaging.jms.management.server"},{"l":"com.sun.messaging.jms.notification"},{"l":"com.sun.messaging.xml"}];updateSearchResults();
